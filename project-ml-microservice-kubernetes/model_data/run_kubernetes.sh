76ba3245ef48a9f902af7499b73f8f0af0208f98
huannv93 <huannguyen7651@gmail.com>
2023-03-27 13:14:52 +0700

    update

--- project-ml-microservice-kubernetes/model_data/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/model_data/run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=huannv93/udacity-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run huannv93app\
+    --image=$dockerpath\
+    --port=80\
+    # --labels="huannv93app"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward huannv93app 8000:80
 
